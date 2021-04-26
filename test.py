fb5ea6f97e5ffdf6bb47edb8fa68a15738f125cb
Kev1venteur <kvin.gillet@gmail.com>
2021-04-26 13:51:25 +0200

    test

--- test.py
+++ test.py
@@ -6,8 +6,15 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/html')
         self.end_headers()
 
-        message = "Hello, World!"
+        message = "Hello, World! Here is a GET response"
+        self.wfile.write(bytes(message, "utf8"))
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        message = "Hello, World! Here is a POST response"
         self.wfile.write(bytes(message, "utf8"))
 
-with HTTPServer(('', 8080), handler) as server:
+with HTTPServer(('', 8000), handler) as server:
     server.serve_forever()
